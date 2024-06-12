3397e2529c4bf08364a8e021e16327d627619d15
guilhermigg <guilhermigg.e@gmail.com>
2024-06-11 21:30:49 -0300

    removed: arquivo desnecessário de mensagem

--- GourmetGame.App/Messages/StartGameMessage.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace GourmetGame.App.Messages;
-
-public class StartGameMessage(String StartGame) : ValueChangedMessage<String>(StartGame);
